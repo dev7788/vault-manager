6bbcd8af5fcd9eea0dfe37237d93ddbe90bc2f0f
dev7788 <devvv7788@gmail.com>
2019-01-25 13:04:14 -0500

    Implemented vault POST.

--- modules/queries.js
+++ modules/queries.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const crypto = require('crypto');
+
 // eslint-disable-next-line prefer-destructuring
 const Pool = require('pg').Pool;
 
@@ -14,7 +16,7 @@ const pool = new Pool({
 const getVaultBySourceId = async (request, response) => {
   const sourceId = parseInt(request.params.sourceId, 10);
   try {
-    const results = await pool.query('SELECT * FROM vault WHERE sourceId = $1', [sourceId]);
+    const results = await pool.query('SELECT * FROM vault WHERE sourceid = $1', [sourceId]);
     if (results.rowCount) {
       const row = results.rows[0];
       if (row.maintenance) {
@@ -32,12 +34,61 @@ const getVaultBySourceId = async (request, response) => {
       response.status(404);
     }
   } catch (error) {
+    response.status(500).send({ error: true });
     throw error;
   }
 };
 
 const createVault = async (request, response) => {
-  response.status(200).json({});
+  const { sourceId } = request.body;
+  try {
+    let result = await pool.query('SELECT * FROM vault WHERE sourceid = $1', [sourceId]);
+    if (result.rowCount) {
+      response.status(409).json({ msg: 'Already exists' });
+      return;
+    }
+
+    result = await pool.query('INSERT INTO vault (sourceid) VALUES ($1) RETURNING id', [sourceId]);
+    // eslint-disable-next-line prefer-destructuring
+    const id = result.rows[0].id;
+
+    const databaseName = `vault_${id}`;
+    const tallyRole = `vault_${id}_tally`;
+    const tallyPassword = crypto.randomBytes(16).toString('hex');
+    const adapterRole = `vault_${id}_adapter`;
+    const adapterPassword = crypto.randomBytes(16).toString('hex');
+    const apiRole = `vault_${id}_api`;
+
+    // Clone database.
+    result = await pool.query('SELECT * FROM settings LIMIT 1');
+    if (result.rowCount === 0) {
+      response.status(500).send({ error: true });
+      return;
+    }
+    const settings = result.rows[0];
+    const nextVaultHostName = settings.nextvaulthostname;
+    const vaultTemplateName = settings.vaulttemplatename;
+
+    const nextPool = new Pool({
+      user: process.env.PGUSER,
+      host: nextVaultHostName,
+      database: vaultTemplateName,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT,
+    });
+
+    await nextPool.query(`CREATE DATABASE ${databaseName} WITH TEMPLATE ${vaultTemplateName}`);
+    await nextPool.query(`CREATE ROLE ${tallyRole} WITH LOGIN PASSWORD '${tallyPassword}'`);
+    await nextPool.query(`CREATE ROLE ${adapterRole} WITH LOGIN PASSWORD '${adapterPassword}'`);
+    await nextPool.query(`CREATE ROLE ${apiRole}`);
+
+    await pool.query('UPDATE vault SET hostname = $1, databasename = $2, tallyrole = $3, tallypassword = $4, adapterrole = $5, adapterpassword = $6 WHERE id = $7', [settings.nextVaultHostName, databaseName, tallyRole, tallyPassword, adapterRole, adapterPassword, parseInt(id, 10)]);
+
+    response.status(201).json({ id });
+  } catch (error) {
+    response.status(500).send({ error: true });
+    throw error;
+  }
 };
 
 module.exports = {
