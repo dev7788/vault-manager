a62aec51d5992bdfd57b3b7f52416a742e309801
dev7788 <devvv7788@gmail.com>
2019-01-28 21:46:21 -0500

    added 2 GET tests.

--- test/vault-manager.spec.js
+++ test/vault-manager.spec.js
@@ -49,9 +49,36 @@ describe('vault-manager', () => {
           .send(testData.vaultInsert)
           .end((err, res) => {
             if (err) done(err);
-            console.log('test1');
             expect(res).to.have.status(201);
             expect(res.body).to.haveOwnProperty('id');
+            done();
+          });
+      });
+
+      it('should get an adapter successfully', (done) => {
+        chai.request(app)
+          .get('/vault/1/connection/adapter')
+          .end((err, res) => {
+            if (err) done(err);
+            expect(res).to.have.status(200);
+            expect(res.body).to.haveOwnProperty('hostname');
+            expect(res.body).to.haveOwnProperty('database');
+            expect(res.body).to.haveOwnProperty('username');
+            expect(res.body).to.haveOwnProperty('password');
+            done();
+          });
+      });
+
+      it('should get a tally successfully', (done) => {
+        chai.request(app)
+          .get('/database/1/connection/tally')
+          .end((err, res) => {
+            if (err) done(err);
+            expect(res).to.have.status(200);
+            expect(res.body).to.haveOwnProperty('hostname');
+            expect(res.body).to.haveOwnProperty('database');
+            expect(res.body).to.haveOwnProperty('username');
+            expect(res.body).to.haveOwnProperty('password');
             done();
           });
       });
